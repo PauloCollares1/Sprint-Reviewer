0b832e40ca91e33ba53dbbb24c3c9de61fffc8ce
Paulo Collares <paulocollares1@hotmail.com>
2023-10-22 00:18:19 -0300

    Configurando o Eslint + prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,14 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
 import { AppThemeProvider } from "./shared/context/themeContext";
 
-export const App = () =>  {
+export const App = () => {
   return (
     <AppThemeProvider>
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
     </AppThemeProvider>
-
   );
-}
+};
 
 export default App;
